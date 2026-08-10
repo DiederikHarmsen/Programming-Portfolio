a11716b5d55e1f02453c2dab0e7987488364da06
DiederikHarmsen <diederik.harmsen@gmail.com>
2026-08-10 12:47:10 +0200

    Update account_repository.py

--- SQLProjects/Python-Bankapp/account_repository.py
+++ SQLProjects/Python-Bankapp/account_repository.py
@@ -1,1 +1,11 @@
 
+class AccountRepository:
+
+    def __init__(self, session):
+        self.session = session
+
+    def find_by_id(self, account_id):
+        statement = select(Account).where(Account.id == account_id)
+        account = self.session.scalars(statement).first()
+
+        return account
